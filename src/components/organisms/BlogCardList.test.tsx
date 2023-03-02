9cb3480bc7a467b7b966789ba03420104ab45f55
RyanJThompson <ryan.thompson2@sky.uk>
2023-03-02 21:30:42 +0000

    cleaned up the logic of BlogCardList unit test

--- src/components/organisms/BlogCardList.test.tsx
+++ src/components/organisms/BlogCardList.test.tsx
@@ -9,45 +9,46 @@ interface DataItem {
   image: ImageURISource;
 }
 
-/*  Each time MockBlogCardElement() is called it will create a new member of the mockData
-    array and when used as a render item prop it will render the BlogCard n number
-    of times where n = the length of the mockData array */
 const mockData: DataItem[] = [];
-let index = -1;
-const MockBlogCardElement = () => {
-  index++;
-  mockData[index] = {
-    id: index + 1,
-    title: `title number ${index + 1}`,
-    description: `description number ${index + 1}`,
-    image: { uri: `https://test-image.com/number-${index + 1}` },
-  };
+const MockBlogCardElement = (numOfTimes: number) => {
+  for (let i = 0; i < numOfTimes; i++) {
+    mockData[i] = {
+      id: i + 1,
+      title: `title number ${i + 1}`,
+      description: `description number ${i + 1}`,
+      image: { uri: `https://test-image.com/number-${i + 1}` },
+    };
+  }
+};
+
+let i = -1;
+const renderItem = () => {
+  i++;
   return (
     <BlogCard
-      title={`title number ${index + 1}`}
-      description={`description number ${index + 1}`}
-      image={{ uri: `https://test-image.com/number-${index + 1}` }}
+      title={`title number ${i + 1}`}
+      description={`description number ${i + 1}`}
+      image={{ uri: `https://test-image.com/number-${i + 1}` }}
     />
   );
 };
 
 describe('Unit - Organisms: BlogCardList', () => {
   it('SHOULD render the Blog Card List with the correct values', () => {
-    [2].forEach(() => {
-      MockBlogCardElement();
-    });
+    MockBlogCardElement(3);
     const { getByTestId } = render(
       <FlatList
         data={mockData}
-        renderItem={MockBlogCardElement}
+        renderItem={renderItem}
         keyExtractor={item => item.id.toString()}
         testID="blog-card-list"
       />,
     );
+
     expect(getByTestId('blog-card-list')).toBeDefined();
     const element = getByTestId('blog-card-list');
     expect(element.props.data[0].title).toBe('title number 1');
     expect(element.props.data[1].description).toBe('description number 2');
-    expect(element.props.data[1].image.uri).toBe('https://test-image.com/number-2');
+    expect(element.props.data[2].image.uri).toBe('https://test-image.com/number-3');
   });
 });
