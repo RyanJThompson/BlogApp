fdf56426aa835f52f61cae2e4605193bde672c02
RyanJThompson <ryan.thompson2@sky.uk>
2023-03-05 18:51:26 +0000

    fixed app error

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,8 @@
  * @format
  */
 
-import { SafeAreaView } from 'react-native-safe-area-context';
 import NavigationContainer from './navigators/NavigationContainer';
 
 export default function App() {
-  return (
-    <SafeAreaView>
-      <NavigationContainer />;
-    </SafeAreaView>
-  );
+  return <NavigationContainer />;
 }
