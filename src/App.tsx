5c70896869bece3040f57899ca397592426d8b0f
RyanJThompson <ryan.thompson2@sky.uk>
2023-03-26 17:15:37 +0100

    refactored app.tsx and nav container

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
 // App.tsx
-import React, { useState, useEffect } from 'react';
-import auth from '@react-native-firebase/auth';
-import { AuthScreen } from './screens';
+import React from 'react';
 import NavigationContainer from './navigators/NavigationContainer';
 
 const App: React.FC = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  useEffect(() => {
-    const unsubscribe = auth().onAuthStateChanged(user => {
-      if (user) {
-        setIsAuthenticated(true);
-      } else {
-        setIsAuthenticated(false);
-      }
-    });
-
-    return () => unsubscribe();
-  }, []);
-
-  return <>{isAuthenticated ? <NavigationContainer /> : <AuthScreen />}</>;
+  return <NavigationContainer />;
 };
 
 export default App;
--- src/navigators/NavigationContainer.tsx
+++ src/navigators/NavigationContainer.tsx
@@ -1,4 +1,5 @@
 import { NavigationContainer as RN_NavigationContainer } from '@react-navigation/native';
+import auth from '@react-native-firebase/auth';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Home, Popular, Latest, Profile, Settings } from '../assets/icons';
 import {
@@ -8,35 +9,57 @@ import {
   ProfileScreenNavigator,
   SettingsScreenNavigator,
 } from '.';
+import { AuthScreen } from './../screens';
+import { useEffect, useState } from 'react';
 
 const NavigationContainer: React.FC = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const Tab = createBottomTabNavigator();
+
+  useEffect(() => {
+    const unsubscribe = auth().onAuthStateChanged(user => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <RN_NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false, unmountOnBlur: true }}>
-        <Tab.Screen name="Tab Home" component={HomeScreenNavigator} options={{ tabBarIcon: Home, title: 'Home' }} />
-        <Tab.Screen
-          name="Tab Most Popular"
-          component={PopularScreenNavigator}
-          options={{ tabBarIcon: Popular, title: 'Most Popular' }}
-        />
-        <Tab.Screen
-          name="Tab Latest"
-          component={LatestScreenNavigator}
-          options={{ tabBarIcon: Latest, title: 'Latest' }}
-        />
-        <Tab.Screen
-          name="Tab Profile"
-          component={ProfileScreenNavigator}
-          options={{ tabBarIcon: Profile, title: 'Profile' }}
-        />
-        <Tab.Screen
-          name="Tab Settings"
-          component={SettingsScreenNavigator}
-          options={{ tabBarIcon: Settings, title: 'Settings' }}
-        />
-      </Tab.Navigator>
-    </RN_NavigationContainer>
+    <>
+      {isAuthenticated ? (
+        <RN_NavigationContainer>
+          <Tab.Navigator screenOptions={{ headerShown: false, unmountOnBlur: true }}>
+            <Tab.Screen name="Tab Home" component={HomeScreenNavigator} options={{ tabBarIcon: Home, title: 'Home' }} />
+            <Tab.Screen
+              name="Tab Most Popular"
+              component={PopularScreenNavigator}
+              options={{ tabBarIcon: Popular, title: 'Most Popular' }}
+            />
+            <Tab.Screen
+              name="Tab Latest"
+              component={LatestScreenNavigator}
+              options={{ tabBarIcon: Latest, title: 'Latest' }}
+            />
+            <Tab.Screen
+              name="Tab Profile"
+              component={ProfileScreenNavigator}
+              options={{ tabBarIcon: Profile, title: 'Profile' }}
+            />
+            <Tab.Screen
+              name="Tab Settings"
+              component={SettingsScreenNavigator}
+              options={{ tabBarIcon: Settings, title: 'Settings' }}
+            />
+          </Tab.Navigator>
+        </RN_NavigationContainer>
+      ) : (
+        <AuthScreen />
+      )}
+    </>
   );
 };
 
